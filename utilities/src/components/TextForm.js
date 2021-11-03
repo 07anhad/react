01843c218e00e88263b66c78d07ff3b78bca10ec
Anhad Parashar <anhadparashar07@gmail.com>
2021-11-04 02:28:33 +0530

    adding logics to textUtils

--- utilities/src/components/TextForm.js
+++ utilities/src/components/TextForm.js
@@ -8,6 +8,12 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = ()=> {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
     const handleOnChange = (event)=> {
         // console.log("On Change");
         setText(event.target.value)
@@ -18,15 +24,24 @@ export default function TextForm(props) {
     // setText("Change the text here") // This is the correct way to update the text in ReactJs 
 
     return (
-        
-        <div>
+        <>
+            <div className = "container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btm btn-primary" onClick={handleUpClick}>Convert To Upercase</button>
-            
+                <button className="btm btn-primary mx-1 my-1" onClick={handleUpClick}>Convert To Upercase</button>
+                <button className="btm btn-primary mx-1 my-1" onClick={handleLoClick}>Convert To Lowercase</button>
             </div>
+
+            <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length - 1} <b>words</b> and {text.length} <b>characters</b></p>
+                <p>{0.008 * (text.split(" ").length - 1)} <b>Minutes read</b></p>
+                <h2>Preview of your text</h2>
+                <p>{text}</p>
+            </div>
+        </>
             
     )
 }
