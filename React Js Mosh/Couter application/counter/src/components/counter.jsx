ee23dd5c15c1e6d09b3cff05170b4ac8c814dd68
Anhad <84476084+07anhad@users.noreply.github.com>
2022-02-07 12:58:56 +0530

    setting attributes

--- React Js Mosh/Couter application/counter/src/components/counter.jsx
+++ React Js Mosh/Couter application/counter/src/components/counter.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 class Counter extends React.Component {
     state = {
-        count:33
+        count: 0,
         
     };
+
+    // styles = {
+    //     fontSize: 20,
+    // };
+
+    //TO APPLY STYLES TO MANY SECTIONS YOU DONT HAVE TO DEFINE MANY STYLE ATTRIBUTES, YOU CAN JUST USE INLINE STYLING
+
     render() {
         return (
-            <React.Fragment>
-                <h1><b>{this.formatCount()}</b></h1>
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                <span style={{fontSize:20}} className="badge badge-primary m-2  "><b>{this.formatCount()}</b></span>
+                <button style={{fontWeight:'bold',fontSize:20}} className="btn btn-secondary btn-sm">Increment</button>
+            </div>
         );
     }
 
