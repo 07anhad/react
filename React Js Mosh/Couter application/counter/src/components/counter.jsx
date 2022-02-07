baf26c043f31cf65ec61823a342b70f37221f731
Anhad <84476084+07anhad@users.noreply.github.com>
2022-02-07 15:16:16 +0530

    Render classes dynamically

--- React Js Mosh/Couter application/counter/src/components/counter.jsx
+++ React Js Mosh/Couter application/counter/src/components/counter.jsx
@@ -12,17 +12,24 @@ class Counter extends React.Component {
     //TO APPLY STYLES TO MANY SECTIONS YOU DONT HAVE TO DEFINE MANY STYLE ATTRIBUTES, YOU CAN JUST USE INLINE STYLING
 
     render() {
+
         return (
             <div>
-                <span style={{fontSize:20}} className="badge badge-primary m-2  "><b>{this.formatCount()}</b></span>
+                <span className = {this.getBadgeClasses()}><b>{this.formatCount()}</b></span>
                 <button style={{fontWeight:'bold',fontSize:20}} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "danger" : "success";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        return count === 0 ? 'ZERO' : count;
     }
 }
 
