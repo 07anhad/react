b937c6b9c5a0ecbfe56ce53b0286a248774fad1c
Anhad <84476084+07anhad@users.noreply.github.com>
2022-02-07 16:13:48 +0530

    Conditional rendering

--- React Js Mosh/Couter application/counter/src/components/counter.jsx
+++ React Js Mosh/Couter application/counter/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 class Counter extends React.Component {
     state = {
         count: 0,
-        
+        tags : ['tag1','tag2','tag3']
     };
 
     // styles = {
@@ -11,25 +11,24 @@ class Counter extends React.Component {
 
     //TO APPLY STYLES TO MANY SECTIONS YOU DONT HAVE TO DEFINE MANY STYLE ATTRIBUTES, YOU CAN JUST USE INLINE STYLING
 
+    // { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+        
+        else return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+                
+    }
+
+
     render() {
 
         return (
             <div>
-                <span className = {this.getBadgeClasses()}><b>{this.formatCount()}</b></span>
-                <button style={{fontWeight:'bold',fontSize:20}} className="btn btn-secondary btn-sm">Increment</button>
+                {this.state.tags.length===0 && "Please add some items"}
+                {this.renderTags()}
             </div>
         );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "danger" : "success";
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'ZERO' : count;
     }
 }
 
