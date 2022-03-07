96ee38cc2ab2218af8fa159de1c43d901dd837ed
Anhad <84476084+07anhad@users.noreply.github.com>
2022-03-07 17:10:36 +0530

    movie land

--- jsm-react-refresher/src/App.js
+++ jsm-react-refresher/src/App.js
@@ -1,23 +1,44 @@
+import {useEffect} from 'react';
+
 import './App.css';
-import { useState,useEffect} from 'react';
+import SearchIcon from './search.svg';
+ 
+const API_URL = 'http://www.omdbapi.com?apikey=25179af4';
 
 const App = () => {
-  
-  const [counter, setcounter] = useState(0);
-  
-  useEffect(() => {
-    alert("You have changed the counter to " + counter);
-  },[counter]);
 
-  return (
-    <div className="App">
-      <br />
-      <button onClick={() => setcounter((prevCount) => prevCount - 1)}> - </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setcounter((prevCount) => prevCount + 1)}> + </button>
-    </div>
-  );
-  
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+        console.log(data.Search);
+    }  
+
+    useEffect(() => {
+        searchMovies('superman')
+    },[])  // GIVES THE DATA WHENEVER THE PAGE IS LOADED
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder='Search for movies...'
+                    value="Superman"
+                    onChange={() => { }}
+                    
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }}
+                    
+                 />
+            </div>
+        </div>
+    );
 }
 
 export default App;
