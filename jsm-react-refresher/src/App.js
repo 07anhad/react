67ac421fed7a0b81cfa07c41bdefb06904cd3cd6
Anhad <84476084+07anhad@users.noreply.github.com>
2022-03-07 16:15:03 +0530

    use state and hooks

--- jsm-react-refresher/src/App.js
+++ jsm-react-refresher/src/App.js
@@ -1,32 +1,20 @@
 import './App.css';
-
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName }</h2>
-      <h2>Age: { props.age}</h2>
-    </>
-  )
-}
+import { useState,useEffect} from 'react';
 
 const App = () => {
+  
+  const [counter, setcounter] = useState(0);
+  
+  useEffect(() => {
+    alert("You have changed the counter to " + counter);
+  },[counter]);
 
   return (
     <div className="App">
-      <Person
-        name='Anhad'
-        lastName='Parashar'
-        age='19'
-      />
-
       <br />
-
-      <Person 
-        name='john' 
-        lastName='doe' 
-        age={17 + 44}
-      />
+      <button onClick={() => setcounter((prevCount) => prevCount - 1)}> - </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setcounter((prevCount) => prevCount + 1)}> + </button>
     </div>
   );
   
